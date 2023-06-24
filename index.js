a8a69fb177b8fb046afe2017c1360d6f989bbf6e
mariana-cx <133844552+mariana-cx@users.noreply.github.com>
2023-06-24 17:09:27 -0300

    loanding

---
+++ index.js
@@ -0,0 +1,13 @@
+function giveExcuse() {
+
+    let who = ['the dog', 'my granma', 'his turtle', 'my bird'];
+    let what = ['eat', 'pissed', 'crushed', 'broked'];
+    let when = ['before the class', 'right in time', 'when I finished', 'during my lunch', 'while I was praying'];
+
+    let randomWho = Math.floor(Math.random()*who.length);
+    let randomWhat = Math.floor(Math.random()*what.length);
+    let randomWhen = Math.floor(Math.random()*when.length);
+
+    document.querySelector("#excuse").innerHTML = who[randomWho] + " " + what[randomWhat] + " " + when[randomWhen];
+
+};
